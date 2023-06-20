a9740fe9528f02c7bc5128783721e910a20a2091
Guilherme Pontual <guilhermeeli2009@hotmail.com>
2023-06-20 11:39:38 -0300

    iot

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import requests
 import pandas as pd
 
+logo_image = "ecognitive.png"
+st.image(logo_image, use_column_width=True)
+
 url = "https://api.thingspeak.com/channels/2121478/fields/1.json"
 response = requests.get(url)
 data = response.json()
@@ -10,7 +13,7 @@ created_at_values = [entry["created_at"] for entry in data["feeds"]]
 field1_values = [float(entry["field1"]) for entry in data["feeds"]]
 
 with st.container():
-    st.subheader("eCOgnitive IoT")
+    st.subheader("Elemento obtido pelo thigspeak: Co2")
     st.title('Gráfico de Quantidade de Poluente')
 
 with st.container():
@@ -18,6 +21,11 @@ with st.container():
     df = pd.DataFrame({"Data": created_at_values, "Quantidade do Poluente": field1_values})
 
     df = df[df["Quantidade do Poluente"] > 0]
+
+    # calcular a porcentagem de poluente em relação ao total
+    total = df["Quantidade do Poluente"].sum()
+    df["Porcentagem"] = (df["Quantidade do Poluente"] / total) * 100
+    #df["Porcentagem"] = df["Porcentagem"].map("{:.2f}%".format)  # formata os valores como porcentagem
 
     period = st.selectbox("Selecione o período", ["7D", "15D", "30D", "MAX", "MIN"])
 
@@ -32,7 +40,7 @@ with st.container():
     elif period == "MIN":
         filtered_df = df.nsmallest(1, "Quantidade do Poluente")
     else:
-        filtered_df = df  # Período completo
+        filtered_df = df
 
     if st.button("Exibir Gráfico"):
-        st.bar_chart(filtered_df, x="Data", y="Quantidade do Poluente")
+        st.bar_chart(filtered_df, x="Data", y="Porcentagem")
